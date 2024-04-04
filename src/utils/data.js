490c17c5e6a73b9823289980975607a85d6de67f
roid zuhdianto <roidzuh@gmail.com>
2024-04-04 09:55:37 +0700

    untuk menyimpan data

---
+++ src/utils/data.js
@@ -0,0 +1,12 @@
+import { HiHome, HiMap } from "react-icons/hi2";
+
+export const navLinks = [
+  { title: "Home", href: "/", icon: HiHome },
+  { title: "Destinations", href: "/", icon: HiMap },
+];
+
+export const loginImage = [
+  { id: 1, image: "/Login-bg.jpg", title: "Login" },
+  { id: 2, image: "/Login-bg-2.jpg", title: "Login" },
+  { id: 3, image: "/Login-bg-3.jpg", title: "Login" },
+];
