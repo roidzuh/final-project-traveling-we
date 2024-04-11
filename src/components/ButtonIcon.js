7ce46c17524fe1352ec6e7a5cc5673e5fdce5d46
roid zuhdianto <roidzuh@gmail.com>
2024-04-11 19:00:26 +0700

    update

--- src/components/ButtonIcon.js
+++ src/components/ButtonIcon.js
@@ -1,7 +1,7 @@
-export default function ButtonIcon({ children, onClick }) {
+export default function ButtonIcon({ children, onClick, style }) {
   return (
     <button
-      className={` flex bg-transparent  border-none transition-all ease-in duration-300 cursor-pointer text-3xl`}
+      className={`${style} flex bg-transparent  border-none transition-all ease-in duration-300 cursor-pointer text-3xl`}
       onClick={onClick}
     >
       {children}
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 bg-black">
+    <footer className="px-4 sm:px-6 lg:px-8 bg-black">
       <div className="border-t border-gray-200 py-8 text-center text-sm text-gray-500">
         Copyright © 2024 Travelo. All rights reserved.
       </div>
