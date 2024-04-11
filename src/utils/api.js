d8f4b1a91d422c1dad5acae66fd640f510661eb0
roid zuhdianto <roidzuh@gmail.com>
2024-04-11 22:14:30 +0700

    update utils (data & api)

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,23 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
-const BASE_URL = "https://travel-journal-api-bootcamp.do.dibimbing.id/";
-const API_KEY = "24405e01-fbc1-45a5-9f5a-be13afcd757c";
+export const BASE_URL = "https://travel-journal-api-bootcamp.do.dibimbing.id/";
+export const API_KEY = "24405e01-fbc1-45a5-9f5a-be13afcd757c";
+
+// export async function fetchUserLogged(token) {
+//   try {
+//     const res = await axios.get(`${BASE_URL}api/v1/user`, {
+//       headers: {
+//         apiKey: API_KEY,
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+//     return res?.data?.data || [];
+//   } catch (error) {
+//     console.error("Error fetching users:", error);
+//     return [];
+//   }
+// }
 
 export async function fetchBanners() {
   try {
@@ -16,3 +32,78 @@ export async function fetchBanners() {
     return [];
   }
 }
+
+export async function fetchPromo() {
+  try {
+    const res = await axios.get(`${BASE_URL}api/v1/promos`, {
+      headers: {
+        apiKey: API_KEY,
+      },
+    });
+    return res?.data?.data || [];
+  } catch (error) {
+    console.error("Error fetching promos:", error);
+    return [];
+  }
+}
+
+export async function fetchCategory() {
+  try {
+    const res = await axios.get(`${BASE_URL}api/v1/categories`, {
+      headers: {
+        apiKey: API_KEY,
+      },
+    });
+    return res?.data?.data || [];
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    return [];
+  }
+}
+
+export async function fetchActivity() {
+  try {
+    const res = await axios.get(`${BASE_URL}api/v1/activities`, {
+      headers: {
+        apiKey: API_KEY,
+      },
+    });
+    return res?.data?.data || [];
+  } catch (error) {
+    console.error("Error fetching activities:", error);
+    return [];
+  }
+}
+
+export const handleLogin = async (email, password) => {
+  try {
+    const response = await axios.post(
+      `${BASE_URL}api/v1/login`,
+      { email, password },
+      {
+        headers: {
+          apiKey: API_KEY,
+        },
+      }
+    );
+    return response?.data;
+  } catch (error) {
+    throw new Error(error.response.data.message || "Login failed");
+  }
+};
+
+// export const handleLogout = async () => {
+//   try {
+//     const response = await axios.get(`${BASE_URL}api/v1/logout`, {
+//       headers: {
+//         apiKey: API_KEY,
+//         Authorization: `Bearer ${Cookies.get("token")}`,
+//       },
+//     });
+//     return response.data;
+//   } catch (error) {
+//     throw new Error(
+//       error.response?.data?.message || "Logout failed: Unexpected error"
+//     );
+//   }
+// };
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,9 @@
 import { HiHome, HiMap } from "react-icons/hi2";
+import { MdLocalOffer } from "react-icons/md";
+import { RiAdvertisementLine } from "react-icons/ri";
+import { BiHomeSmile } from "react-icons/bi";
+import { AiOutlineUser } from "react-icons/ai";
+import { BsGrid3X3Gap, BsCalendar } from "react-icons/bs";
 
 export const navLinks = [
   { title: "Home", href: "/", icon: HiHome },
@@ -9,4 +14,13 @@ export const loginImage = [
   { id: 1, image: "/Login-bg.jpg", title: "Login" },
   { id: 2, image: "/Login-bg-2.jpg", title: "Login" },
   { id: 3, image: "/Login-bg-3.jpg", title: "Login" },
+];
+
+export const sidebarLink = [
+  { title: "Home", href: "/dashboard", icon: BiHomeSmile },
+  { title: "User", href: "/dashboard/user", icon: AiOutlineUser },
+  { title: "Banner", href: "/dashboard/banner", icon: RiAdvertisementLine },
+  { title: "Promo", href: "/dashboard/promo", icon: MdLocalOffer },
+  { title: "Category", href: "/dashboard/category", icon: BsGrid3X3Gap },
+  { title: "Activity", href: "/dashboard/activity", icon: BsCalendar },
 ];
