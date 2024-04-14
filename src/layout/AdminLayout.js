b8b1642bd8ea5b9af274c03abb5398ca5f620c2d
roid zuhdianto <roidzuh@gmail.com>
2024-04-14 15:47:54 +0700

    update layout admin

--- src/layout/AdminLayout.js
+++ src/layout/AdminLayout.js
@@ -1,14 +1,13 @@
+import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
-import User from "@/components/User";
 
 export default function AdminLayout({ children, user }) {
   return (
-    <div className="grid w-[96%] my-0 mx-auto gap-7 grid-cols-[12rem_auto_23rem]">
+    <div className="grid w-[96%] my-0 mx-auto gap-7 grid-cols-[12rem_auto] grid-rows-[1rem_auto]">
+      <Header />
       <Sidebar />
       <main>{children}</main>
-      <div className="flex justify-end gap-8 mt-4">
-        {/* <User user={user} /> */}
-      </div>
+      <div className="flex justify-end gap-8 mt-4"></div>
     </div>
   );
 }
