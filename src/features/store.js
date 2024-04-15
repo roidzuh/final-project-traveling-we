0b39d794f936c0f1a12f60c9c1cda0e66c8b5b0b
roid zuhdianto <roidzuh@gmail.com>
2024-04-15 07:43:58 +0700

    update redux untuk mengambil data user

--- src/features/store.js
+++ src/features/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import navbarReducer from "./slices/navbarSlice";
+import userReducer from "./slices/userSlice";
 
 const store = configureStore({
   reducer: {
     navbar: navbarReducer,
+    user: userReducer,
   },
 });
 
