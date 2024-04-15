1b740297afa04ee2caad2fd24d78b7c03a2d2c04
roid zuhdianto <roidzuh@gmail.com>
2024-04-15 08:13:36 +0700

    update styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import User from "./User";
 
 export default function Header() {
   return (
-    <div className="col-span-full flex justify-between">
-      <h1 className="text-2xl mt-2 font-bold cursor-default">TravelGo</h1>
+    <div className="col-span-full flex justify-between items-center mt-10">
+      <h1 className="text-2xl font-bold cursor-default">TravelGo</h1>
       <User />
     </div>
   );
--- src/components/User.js
+++ src/components/User.js
@@ -5,9 +5,18 @@ export default function User() {
   const user = useSelector((state) => state.user.data);
 
   return (
-    <div>
-      <h1>{user?.name}</h1>
-      <p>{user?.email}</p>
+    <div className="flex gap-4 items-center">
+      <div>
+        <h1 className="text-xl font-bold">{user?.name}</h1>
+        <p className="text-sm text-gray-500">{user?.email}</p>
+      </div>
+      <div>
+        <img
+          src={user?.profilePictureUrl}
+          alt="avatar"
+          className="w-14 h-14 rounded-full"
+        />
+      </div>
     </div>
   );
 }
