590b953330cc2874e8dc5b8db3706c965657bf2f
roid zuhdianto <roidzuh@gmail.com>
2024-04-15 07:41:25 +0700

    update styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import User from "./User";
+
 export default function Header() {
   return (
-    <div className="col-span-full">
+    <div className="col-span-full flex justify-between">
       <h1 className="text-2xl mt-2 font-bold cursor-default">TravelGo</h1>
+      <User />
     </div>
   );
 }
