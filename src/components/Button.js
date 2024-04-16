e909df5336e45b429fb2cc2bad2da4246140a5ca
roid zuhdianto <roidzuh@gmail.com>
2024-04-16 15:55:05 +0700

    update button componen

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,16 @@
-export default function Button({ title, onClick, style, type = "button" }) {
+export default function Button({
+  title,
+  onClick,
+  style,
+  type = "button",
+  isLoading,
+}) {
   return (
     <button
       type={type}
-      className={`${style} px-4 py-2 rounded-md border-none transition-all ease-in duration-300 cursor-pointer`}
+      className={` px-4 py-2 rounded-md border-none transition-all ease-in duration-300 cursor-pointer ${style}`}
       onClick={onClick}
+      disabled={isLoading}
     >
       {title}
     </button>
