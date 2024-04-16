90c8de6560f27cb6caf4ee3bd7157f74ec6557a7
roid zuhdianto <roidzuh@gmail.com>
2024-04-16 07:51:25 +0700

    update styling user login

--- src/components/User.js
+++ src/components/User.js
@@ -1,15 +1,24 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { fetchUser } from "@/utils/api";
+import ButtonIcon from "./ButtonIcon";
+import { HiMiniUser } from "react-icons/hi2";
+import { useRouter } from "next/router";
 
 export default function User() {
-  const user = useSelector((state) => state.user.data);
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const getUser = async () => {
+      const fetchedUser = await fetchUser(token);
+      setUser(fetchedUser?.data);
+    };
+    getUser();
+  }, []);
 
   return (
     <div className="flex gap-4 items-center">
-      <div>
-        <h1 className="text-xl font-bold">{user?.name}</h1>
-        <p className="text-sm text-gray-500">{user?.email}</p>
-      </div>
       <div>
         <img
           src={user?.profilePictureUrl}
@@ -17,6 +26,13 @@ export default function User() {
           className="w-14 h-14 rounded-full"
         />
       </div>
+      <div>
+        <h1 className="text-xl font-bold">{user?.name}</h1>
+        <p className="text-sm text-gray-500">{user?.email}</p>
+      </div>
+      <ButtonIcon onClick={() => router.push("/dashboard/profile")}>
+        <HiMiniUser className="w-6 h-6" />
+      </ButtonIcon>
     </div>
   );
 }
