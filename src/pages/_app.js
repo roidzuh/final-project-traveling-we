ac143a1ab4bb51744b9eaca67fdb7472602c4c93
roid zuhdianto <roidzuh@gmail.com>
2024-04-16 15:53:19 +0700

    konfigurasi react toastify

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
 import store from "@/features/store";
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
+      <ToastContainer position="top-center" />
     </Provider>
   );
 }
