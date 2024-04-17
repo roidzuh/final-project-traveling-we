1177363046aad1ecfa734b70547e0f7132d69e67
roid zuhdianto <roidzuh@gmail.com>
2024-04-17 16:03:14 +0700

    konfigurasi css bootstrap

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }) {
   return (
