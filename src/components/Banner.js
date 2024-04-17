2115a985a3b9134de3c6ec095eccf49a0d88b628
roid zuhdianto <roidzuh@gmail.com>
2024-04-17 20:05:52 +0700

    update banner componen

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,30 +3,29 @@ import Link from "next/link";
 
 export default function Banner({ banners }) {
   return (
-    <div className="bg-white text-gray-600">
+    <div className="bg-white text-gray-600 w-full">
       <div className="flex flex-col items-center justify-center gap-4 px-4 sm:px-6 lg:px-8 py-6 sm:py-8 lg:py-10">
         <div className="max-w-3xl text-center">
           <h2>Popular Destinations</h2>
           <p>&quot;Find your best experience&quot;</p>
         </div>
         <div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+          <div className="flex gap-4 flex-wrap justify-center">
             {banners.map((banner) => (
-              <Link href={`/banners/${banner.id}`} key={banner.id}>
-                <Card style={{ width: "18rem", position: "relative" }}>
-                  <Card.Img
-                    variant="top"
-                    src={banner.imageUrl}
-                    alt={banner.name}
-                    className="w-full h-48 object-cover rounded-lg"
-                  />
-                  <div className="card-overlay absolute inset-0 rounded-lg bg-black bg-opacity-50 flex justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300">
-                    <Card.Body className="text-white text-center">
-                      <Card.Title>{banner.name}</Card.Title>
-                    </Card.Body>
-                  </div>
-                </Card>
-              </Link>
+              <div
+                className="card relative"
+                style={{ width: "18rem" }}
+                key={banner.id}
+              >
+                <img
+                  src={banner.imageUrl}
+                  alt={banner.name}
+                  className="w-full h-52 object-cover rounded-md transition duration-300 ease-in-out hover:blur-sm"
+                />
+                <div className="card-body absolute w-full h-full top-0 flex items-center justify-center opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-black bg-opacity-50 rounded-md">
+                  <h5 className="text-white text-center mb-4">{banner.name}</h5>
+                </div>
+              </div>
             ))}
           </div>
         </div>
