b59d828358802de1fa484573eae8d1d4cee7b275
roid zuhdianto <roidzuh@gmail.com>
2024-04-18 11:06:13 +0700

    update admin layout

--- src/layout/AdminLayout.js
+++ src/layout/AdminLayout.js
@@ -2,16 +2,23 @@ import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import { isAuthenticated } from "@/utils/auth";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function AdminLayout({ children, user }) {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (!isAuthenticated()) {
       router.push("/login");
+    } else {
+      setLoading(false);
     }
   }, [router]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="grid w-[96%] my-0 mx-auto gap-7 grid-cols-[12rem_auto] grid-rows-[1rem_auto]">
