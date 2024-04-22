d79e6fb9d88d6601c3fc32983d794e3f012c77c6
roid zuhdianto <roidzuh@gmail.com>
2024-04-22 19:18:04 +0700

    tambah css react slick

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,8 @@ import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function App({ Component, pageProps }) {
   return (
