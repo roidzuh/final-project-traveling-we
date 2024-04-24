1ed1f904a81d6765bb0ca95783c24e2902e497fb
roid zuhdianto <roidzuh@gmail.com>
2024-04-24 23:42:04 +0700

    Buat componen spinner untuk loading

---
+++ src/components/Spinners.js
@@ -0,0 +1,12 @@
+export default function Spinners() {
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+}
