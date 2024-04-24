6fbc737cd69938cf123e6d715991f7b9d4788721
roid zuhdianto <roidzuh@gmail.com>
2024-04-24 23:41:06 +0700

    update componen button icon

--- src/components/ButtonIcon.js
+++ src/components/ButtonIcon.js
@@ -1,8 +1,9 @@
-export default function ButtonIcon({ children, onClick, style }) {
+export default function ButtonIcon({ children, onClick, style, disabled }) {
   return (
     <button
-      className={`${style} tw-flex tw-bg-transparent tw-border-none tw-transition-all tw-ease-in tw-duration-300 tw-cursor-pointer tw-text-3xl`}
+      className={`tw-flex tw-bg-transparent tw-border-none tw-transition-all tw-ease-in tw-duration-300 tw-cursor-pointer tw-text-3xl ${style}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
