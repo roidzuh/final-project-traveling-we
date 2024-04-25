c4e6c883c0c784ec8e8ccc8ddba68066bcd180b0
roid zuhdianto <roidzuh@gmail.com>
2024-04-25 18:29:46 +0700

    ubah fetching datanya

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import MainLayout from "@/layout/MainLayout";
 import Hero from "@/components/Hero";
 import {
@@ -11,52 +10,38 @@ import Banner from "@/components/Banner";
 import Category from "@/components/Category";
 import Promo from "@/components/Promo";
 import Activity from "@/components/Activity";
-import Spinners from "@/components/Spinners";
 
-export default function Home() {
-  const [banners, setBanners] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [promos, setPromos] = useState([]);
-  const [activities, setActivities] = useState([]);
-  const [loading, setLoading] = useState(true);
+export async function getServerSideProps() {
+  try {
+    const dataBanners = await fetchBanners();
+    const banners = dataBanners?.data || [];
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const dataBanners = await fetchBanners();
-        setBanners(dataBanners?.data);
+    const dataCategory = await fetchCategory();
+    const categories = dataCategory?.data || [];
 
-        const dataCategory = await fetchCategory();
-        setCategories(dataCategory?.data);
+    const dataPromo = await fetchPromo();
+    const promos = dataPromo?.data || [];
 
-        const dataPromo = await fetchPromo();
-        setPromos(dataPromo?.data);
+    const dataActivity = await fetchActivity();
+    const activities = dataActivity?.data || [];
 
-        const dataActivity = await fetchActivity();
-        setActivities(dataActivity?.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
+    return { props: { banners, categories, promos, activities } };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: { banners: [], categories: [], promos: [], activities: [] },
     };
+  }
+}
 
-    fetchData();
-  }, []);
-
+export default function Home({ banners, categories, promos, activities }) {
   return (
     <MainLayout>
-      {loading ? (
-        <Spinners />
-      ) : (
-        <>
-          <Hero />
-          <Category categories={categories} />
-          <Promo promos={promos} />
-          <Banner banners={banners} />
-          <Activity activities={activities} />
-        </>
-      )}
+      <Hero />
+      <Category categories={categories} />
+      <Promo promos={promos} />
+      <Banner banners={banners} />
+      <Activity activities={activities} />
     </MainLayout>
   );
 }
