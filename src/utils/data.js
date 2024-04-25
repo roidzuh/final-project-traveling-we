ad2821590dd8d9cad0bb12864a84ecbc6d1a8666
roid zuhdianto <roidzuh@gmail.com>
2024-04-25 21:35:56 +0700

    update path image

--- src/utils/data.js
+++ src/utils/data.js
@@ -14,7 +14,7 @@ export const navLinks = [
 export const loginImage = [
   { id: 1, image: "/Login-bg.jpg", title: "Login" },
   { id: 2, image: "/login-bg-2.jpg", title: "Login" },
-  { id: 3, image: "/Login-bg-3.jpg", title: "Login" },
+  { id: 3, image: "/login-bg-3.jpg", title: "Login" },
 ];
 
 export const sidebarLink = [
